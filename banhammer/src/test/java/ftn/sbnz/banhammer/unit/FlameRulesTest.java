39b8e59dedeec6e33f0020b48b3ff64c54fe1fa7
Milan Milovanovic <m.milovanovic@outlook.com>
2020-06-09 21:54:55 +0200

    added tests for flame and hate

---
+++ banhammer/src/test/java/ftn/sbnz/banhammer/unit/FlameRulesTest.java
@@ -0,0 +1,264 @@
+package ftn.sbnz.banhammer.unit;
+
+import ftn.sbnz.banhammer.model.*;
+import org.junit.jupiter.api.Test;
+import org.kie.api.KieServices;
+import org.kie.api.runtime.KieContainer;
+import org.kie.api.runtime.KieSession;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class FlameRulesTest {
+    private KieSession createKieSession(){
+        KieServices ks = KieServices.Factory.get();
+        KieContainer kContainer = ks.newKieContainer(ks.newReleaseId("sbnz.integracija", "drools-spring-kjar", "0.0.1-SNAPSHOT"));
+        return kContainer.newKieSession("Tim4KSession");
+    }
+
+    private User createUser(){
+        User user = new User();
+        user.setId(1L);
+        user.setUsername("John");
+        return user;
+    }
+
+    @Test
+    public void noReportsForFlame(){
+        KieSession kieSession = createKieSession();
+        User matchUser = createUser();
+        MatchInfo matchInfo = new MatchInfo(1L, matchUser.getUsername(), true, Report.NONE);
+        MatchEvent matchEvent = new MatchEvent(matchInfo);
+        kieSession.insert(matchUser);
+        kieSession.insert(matchEvent);
+        int rulesActivated = kieSession.fireAllRules();
+        assertThat(rulesActivated).isEqualTo(1);
+        assertThat(matchUser.getPunishment()).isEqualTo(Punishment.NONE);
+        assertThat(matchUser.getThreatLevel()).isEqualTo(ThreatLevel.NONE);
+    }
+
+    @Test
+    public void reportForFlameEmptyMatchHistory(){
+        KieSession kieSession = createKieSession();
+        User matchUser = createUser();
+        MatchInfo matchInfo = new MatchInfo(1L, matchUser.getUsername(), true, Report.OFFENSIVE_LANGUAGE);
+        MatchEvent matchEvent = new MatchEvent(matchInfo);
+        kieSession.insert(matchUser);
+        kieSession.insert(matchEvent);
+        int rulesActivated = kieSession.fireAllRules();
+        assertThat(rulesActivated).isEqualTo(1);
+        assertThat(matchUser.getPunishment()).isEqualTo(Punishment.NONE);
+        assertThat(matchUser.getThreatLevel()).isEqualTo(ThreatLevel.NONE);
+    }
+
+    @Test
+    public void reportForFlameGoodMatchHistory(){
+        KieSession kieSession = createKieSession();
+        User matchUser = createUser();
+
+        MatchInfo matchInfo1 = new MatchInfo(1L, matchUser.getUsername(), true, Report.NONE);
+        MatchInfo matchInfo2 = new MatchInfo(2L, matchUser.getUsername(), true, Report.NONE);
+        MatchInfo matchInfo3 = new MatchInfo(3L, matchUser.getUsername(), true, Report.NONE);
+        MatchInfo matchInfo4 = new MatchInfo(4L, matchUser.getUsername(), true, Report.OFFENSIVE_LANGUAGE);
+        MatchEvent matchEvent1 = new MatchEvent(matchInfo1);
+        MatchEvent matchEvent2 = new MatchEvent(matchInfo2);
+        MatchEvent matchEvent3 = new MatchEvent(matchInfo3);
+        MatchEvent matchEvent4 = new MatchEvent(matchInfo4);
+
+        // insert first match
+        kieSession.insert(matchUser);
+        kieSession.insert(matchEvent1);
+        kieSession.fireAllRules();
+
+        // insert second match
+        kieSession.insert(matchUser);
+        kieSession.insert(matchEvent2);
+        kieSession.fireAllRules();
+
+        // insert third match
+        kieSession.insert(matchUser);
+        kieSession.insert(matchEvent3);
+        kieSession.fireAllRules();
+
+        // insert fourth match
+        kieSession.insert(matchUser);
+        kieSession.insert(matchEvent4);
+        int rulesActivated = kieSession.fireAllRules();
+        assertThat(rulesActivated).isEqualTo(1);
+        assertThat(matchUser.getPunishment()).isEqualTo(Punishment.NONE);
+        assertThat(matchUser.getThreatLevel()).isEqualTo(ThreatLevel.NONE);
+    }
+
+    @Test
+    public void reportForFlameAndReportInLastFiveMatchesAndChatLogTrue(){
+        KieSession kieSession = createKieSession();
+        User matchUser = createUser();
+
+        MatchInfo matchInfo1 = new MatchInfo(1L, matchUser.getUsername(), true, Report.LEAVING_THE_GAME);
+        MatchInfo matchInfo2 = new MatchInfo(2L, matchUser.getUsername(), true, Report.NONE);
+        MatchInfo matchInfo3 = new MatchInfo(3L, matchUser.getUsername(), true, Report.NONE);
+        MatchInfo matchInfo4 = new MatchInfo(4L, matchUser.getUsername(), true, Report.OFFENSIVE_LANGUAGE);
+        MatchEvent matchEvent1 = new MatchEvent(matchInfo1);
+        MatchEvent matchEvent2 = new MatchEvent(matchInfo2);
+        MatchEvent matchEvent3 = new MatchEvent(matchInfo3);
+        MatchEvent matchEvent4 = new MatchEvent(matchInfo4);
+
+        // insert chat log analysis
+        ChatLogAnalyzer chatLogAnalyzer = new ChatLogAnalyzer();
+        chatLogAnalyzer.setFlame(true);
+        kieSession.insert(chatLogAnalyzer);
+
+        // insert first match
+        kieSession.insert(matchUser);
+        kieSession.insert(matchEvent1);
+        kieSession.fireAllRules();
+
+        // insert second match
+        kieSession.insert(matchUser);
+        kieSession.insert(matchEvent2);
+        kieSession.fireAllRules();
+
+        // insert third match
+        kieSession.insert(matchUser);
+        kieSession.insert(matchEvent3);
+        kieSession.fireAllRules();
+
+        // insert fourth match
+        kieSession.insert(matchUser);
+        kieSession.insert(matchEvent4);
+        int rulesActivated = kieSession.fireAllRules();
+        assertThat(rulesActivated).isEqualTo(4);
+        assertThat(matchUser.getPunishment()).isEqualTo(Punishment.WARNING);
+        assertThat(matchUser.getThreatLevel()).isEqualTo(ThreatLevel.LOW);
+    }
+
+    @Test
+    public void reportForFlameAndReportInLastFiveMatchesAndChatLogFalse(){
+        KieSession kieSession = createKieSession();
+        User matchUser = createUser();
+
+        MatchInfo matchInfo1 = new MatchInfo(1L, matchUser.getUsername(), true, Report.LEAVING_THE_GAME);
+        MatchInfo matchInfo2 = new MatchInfo(2L, matchUser.getUsername(), true, Report.NONE);
+        MatchInfo matchInfo3 = new MatchInfo(3L, matchUser.getUsername(), true, Report.NONE);
+        MatchInfo matchInfo4 = new MatchInfo(4L, matchUser.getUsername(), true, Report.OFFENSIVE_LANGUAGE);
+        MatchEvent matchEvent1 = new MatchEvent(matchInfo1);
+        MatchEvent matchEvent2 = new MatchEvent(matchInfo2);
+        MatchEvent matchEvent3 = new MatchEvent(matchInfo3);
+        MatchEvent matchEvent4 = new MatchEvent(matchInfo4);
+
+        // insert chat log analysis
+        ChatLogAnalyzer chatLogAnalyzer = new ChatLogAnalyzer();
+        chatLogAnalyzer.setFlame(false);
+        kieSession.insert(chatLogAnalyzer);
+
+        // insert first match
+        kieSession.insert(matchUser);
+        kieSession.insert(matchEvent1);
+        kieSession.fireAllRules();
+
+        // insert second match
+        kieSession.insert(matchUser);
+        kieSession.insert(matchEvent2);
+        kieSession.fireAllRules();
+
+        // insert third match
+        kieSession.insert(matchUser);
+        kieSession.insert(matchEvent3);
+        kieSession.fireAllRules();
+
+        // insert fourth match
+        kieSession.insert(matchUser);
+        kieSession.insert(matchEvent4);
+        int rulesActivated = kieSession.fireAllRules();
+        assertThat(rulesActivated).isEqualTo(2);
+        assertThat(matchUser.getPunishment()).isEqualTo(Punishment.NONE);
+        assertThat(matchUser.getThreatLevel()).isEqualTo(ThreatLevel.NONE);
+    }
+
+    @Test
+    public void increasingThreatLevelTest(){
+        KieSession kieSession = createKieSession();
+        User matchUser = createUser();
+        matchUser.setThreatLevel(ThreatLevel.MEDIUM);
+
+        MatchInfo matchInfo1 = new MatchInfo(1L, matchUser.getUsername(), true, Report.LEAVING_THE_GAME);
+        MatchInfo matchInfo2 = new MatchInfo(2L, matchUser.getUsername(), true, Report.NONE);
+        MatchInfo matchInfo3 = new MatchInfo(3L, matchUser.getUsername(), true, Report.NONE);
+        MatchInfo matchInfo4 = new MatchInfo(4L, matchUser.getUsername(), true, Report.OFFENSIVE_LANGUAGE);
+        MatchEvent matchEvent1 = new MatchEvent(matchInfo1);
+        MatchEvent matchEvent2 = new MatchEvent(matchInfo2);
+        MatchEvent matchEvent3 = new MatchEvent(matchInfo3);
+        MatchEvent matchEvent4 = new MatchEvent(matchInfo4);
+
+        // insert chat log analysis
+        ChatLogAnalyzer chatLogAnalyzer = new ChatLogAnalyzer();
+        chatLogAnalyzer.setFlame(true);
+        kieSession.insert(chatLogAnalyzer);
+
+        // insert first match
+        kieSession.insert(matchUser);
+        kieSession.insert(matchEvent1);
+        kieSession.fireAllRules();
+
+        // insert second match
+        kieSession.insert(matchUser);
+        kieSession.insert(matchEvent2);
+        kieSession.fireAllRules();
+
+        // insert third match
+        kieSession.insert(matchUser);
+        kieSession.insert(matchEvent3);
+        kieSession.fireAllRules();
+
+        // insert fourth match
+        kieSession.insert(matchUser);
+        kieSession.insert(matchEvent4);
+        int rulesActivated = kieSession.fireAllRules();
+        assertThat(rulesActivated).isEqualTo(4);
+        assertThat(matchUser.getPunishment()).isEqualTo(Punishment.CHAT_BAN_7_DAYS);
+        assertThat(matchUser.getThreatLevel()).isEqualTo(ThreatLevel.HIGH);
+    }
+
+    @Test
+    public void salienceTestWithConflictingAfk(){
+        KieSession kieSession = createKieSession();
+        User matchUser = createUser();
+        matchUser.setThreatLevel(ThreatLevel.LOW);
+
+        MatchInfo matchInfo1 = new MatchInfo(1L, matchUser.getUsername(), true, Report.LEAVING_THE_GAME);
+        MatchInfo matchInfo2 = new MatchInfo(2L, matchUser.getUsername(), true, Report.NONE);
+        MatchInfo matchInfo3 = new MatchInfo(3L, matchUser.getUsername(), true, Report.NONE);
+        MatchInfo matchInfo4 = new MatchInfo(4L, matchUser.getUsername(), false, Report.OFFENSIVE_LANGUAGE);
+        MatchEvent matchEvent1 = new MatchEvent(matchInfo1);
+        MatchEvent matchEvent2 = new MatchEvent(matchInfo2);
+        MatchEvent matchEvent3 = new MatchEvent(matchInfo3);
+        MatchEvent matchEvent4 = new MatchEvent(matchInfo4);
+
+        // insert chat log analysis
+        ChatLogAnalyzer chatLogAnalyzer = new ChatLogAnalyzer();
+        chatLogAnalyzer.setFlame(true);
+        kieSession.insert(chatLogAnalyzer);
+
+        // insert first match
+        kieSession.insert(matchUser);
+        kieSession.insert(matchEvent1);
+        kieSession.fireAllRules();
+
+        // insert second match
+        kieSession.insert(matchUser);
+        kieSession.insert(matchEvent2);
+        kieSession.fireAllRules();
+
+        // insert third match
+        kieSession.insert(matchUser);
+        kieSession.insert(matchEvent3);
+        kieSession.fireAllRules();
+
+        // insert fourth match
+        kieSession.insert(matchUser);
+        kieSession.insert(matchEvent4);
+        int rulesActivated = kieSession.fireAllRules();
+        assertThat(rulesActivated).isEqualTo(4);
+        assertThat(matchUser.getPunishment()).isEqualTo(Punishment.CHAT_BAN_3_DAYS);
+        assertThat(matchUser.getThreatLevel()).isEqualTo(ThreatLevel.MEDIUM);
+    }
+}
